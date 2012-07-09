76be47a687f8b102c53f7c6e486ba3b1234363c7
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-08 20:17:52 -0500

    Add TODO list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -15,15 +15,16 @@ class ListsController < ApplicationController
     @list = List.new(params[:list])
     if @list.save
       flash[:notice] = "List Created."
-      respond_with(@list, location: list_path(@list))
+      respond_with(@list, location: list_url(@list))
     else
       flash[:error] = "Could not create a list."
-      redirect_to new_list_path(@list)
+      redirect_to new_list_url(@list)
     end
   end
 
   def show
-    respond_with(@list = List.find(params[:id]))
+    @list = List.find(params[:id])
+    @task = @list.tasks.new
   end
 
   def edit
@@ -34,10 +35,10 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     if @list.update_attributes(params[:list])
       flash[:notice] = "List Updated."
-      respond_with(@list, location: list_path(@list))
+      respond_with(@list, location: list_url(@list))
     else
       flash[:error] = "Could not update a list"
-      redirect_to edit_list_path(@list)
+      redirect_to edit_list_url(@list)
     end
   end
 
@@ -45,10 +46,10 @@ class ListsController < ApplicationController
     @list = List.find(params[:id])
     if @list.destroy
       flash[:notice] = "List Deleted."
-      redirect_to lists_path
+      redirect_to lists_url
     else
       flash[:error] = "Could not delete the list, Have you done everything?"
-      redirect_to lists_path
+      redirect_to lists_url
     end
   end
 
--- app/models/list.rb
+++ app/models/list.rb
@@ -1,4 +1,5 @@
 class List < ActiveRecord::Base
   attr_accessible :description, :name
   validates :name, presence: :true
+  has_many :tasks
 end
