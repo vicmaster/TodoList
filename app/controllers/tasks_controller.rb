9d5c4436fb3917b0ed9b4a6c1ce21c59b4f72ab0
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-08 20:19:13 -0500

    Add tasks WIP

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+  attr_accessor :completed
+  before_filter :find_list
+  respond_to :html, :xml, :js
+  layout 'todo'
+
+  def create
+    @task = @list.tasks.new(params[:task])
+    if @task.save
+      flash[:notice] = "Task Created."
+      redirect_to list_url(@list)
+    else
+      flash[:error] = "Could not add a task at this time."
+      redirect_to list_url(@list)
+    end
+  end
+
+  def complete
+    @list = List.find(params[:list_id])
+    @task = @list.tasks.find(params[:id])
+    @task.completed = true
+    @task.save
+    redirect_to list_url(@list)
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,7 @@
 class Task < ActiveRecord::Base
   attr_accessible :completed, :description, :list_id
+  belongs_to :list
+  validates :description, presence: :true
+  scope :completed, where(completed: :true)
+  scope :incompleted, where(incompleted: :false)
 end
