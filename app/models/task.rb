af244c73a529cdbd396e5e27423f1b32b2aa2a3c
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-08 21:24:42 -0500

    Add a task to the TODO list

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,6 @@ class Task < ActiveRecord::Base
   attr_accessible :completed, :description, :list_id
   belongs_to :list
   validates :description, presence: :true
-  scope :completed, where(completed: :true)
-  scope :incompleted, where(incompleted: :false)
+  scope :completed, where(completed: true)
+  scope :incompleted, where(completed: false)
 end
