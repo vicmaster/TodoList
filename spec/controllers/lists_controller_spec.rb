db0748639206f316b77121f18fe1ecc454aed413
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-09 18:29:12 -0500

    WIP test ListsController

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,4 +2,30 @@ require 'spec_helper'
 
 describe ListsController do
 
+  describe "Get Index" do
+    specify do
+      List.should_receive(:all)
+      get :index
+      response.should be_successful
+    end
+  end
+
+  describe "Get New" do
+    specify do
+      List.should_receive(:new)
+      get :new
+      response.should be_successful
+    end
+  end
+
+ # describe "Post Create" do
+ #   let(:list){}
+ #   before do
+ #     List.should_receive(:new).with(list)
+ #   end
+ #   specify do
+ #     post :Create
+ #     response.should be_successful
+ #   end
+ # end
 end
