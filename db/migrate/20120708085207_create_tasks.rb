fea90c835133d58dad49767ffa42a1f59836581f
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-08 20:16:14 -0500

    Add migrations and new schema

--- db/migrate/20120708085207_create_tasks.rb
+++ db/migrate/20120708085207_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :description
-      t.boolean :completed
+      t.boolean :completed, default: :false
       t.integer :list_id
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120707201101) do
+ActiveRecord::Schema.define(:version => 20120708085207) do
 
   create_table "lists", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "tasks", :force => true do |t|
+    t.string   "description"
+    t.boolean  "completed",   :default => false
+    t.integer  "list_id"
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
   end
 
 end
