0a92d6d17cc2dae29b7798f8b84967e8e4600a61
Victor Velazquez <velazquezgaspar16@hotmail.com>
2012-07-11 18:28:40 -0500

    Add delete spec

--- spec/controllers/lists_controller_spec.rb
+++ spec/controllers/lists_controller_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe ListsController do
 
-  describe "Get Index" do
+  describe "Get: Index" do
     specify do
       List.should_receive(:all)
       get :index
@@ -10,7 +10,7 @@ describe ListsController do
     end
   end
 
-  describe "Get New" do
+  describe "Get: New" do
     specify do
       List.should_receive(:new)
       get :new
@@ -18,14 +18,62 @@ describe ListsController do
     end
   end
 
- # describe "Post Create" do
- #   let(:list){}
- #   before do
- #     List.should_receive(:new).with(list)
- #   end
- #   specify do
- #     post :Create
- #     response.should be_successful
- #   end
- # end
+  describe "Delete: Destroy" do
+    let(:list) { mock List }
+
+    before do
+      List.should_receive(:find).and_return list
+      delete :destroy, id: 1
+    end
+
+    specify do
+      response.should be_redirect(lists_url)
+    end
+
+    specify do
+      flash[:notice].should_not be_nil
+    end
+
+  end
+
+=begin
+  describe "Post: Create" do
+    before do
+      List.should_receive(:new)
+    end
+
+    context 'When create OK' do
+      before do
+        List.any_instance.should_receive(:save).and_return true
+        post :create
+        response.should be_successful
+      end
+
+      it 'should redirect to index' do
+        response.should respond_to(list_path)
+      end
+
+      it 'should set error message' do
+        flash[:notice].should_not be_nil
+      end
+    end
+
+    context 'When create error' do
+      before do
+        List.any_instance.should_receive(:save)
+        post :create
+      end
+
+      it 'should redirect to new' do
+        response.should redirect_to(new_list_path)
+      end
+
+      it 'should set error message' do
+        flash[:error].should_not be_nil
+      end
+    end
+  end
+=end
+
+
 end
