20f8d2cab9807b4bef9d29f902f86263c5965de5
Victor Velazquez <victor.velazquez@crowdint.com>
2012-08-21 04:32:45 -0500

    Add task spec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -2,4 +2,37 @@ require 'spec_helper'
 
 describe TasksController do
 
+  describe "Post: Create" do
+    let(:task) { mock Task, description: 'Clean all fornitures' }
+    let(:list) { mock 'list' }
+    before do
+      Task.stub_chain(:tasks, :new).and_return task
+    end
+
+    context 'When create not fail' do
+      before do
+        task.should_receive(:save).and_return true
+        post :create
+      end
+
+      specify do
+        response.should be_success
+        flash[:notice] = "Task Created."
+      end
+    end
+
+    context 'When create fail' do
+      before do
+        task.should_receive(:save).and_return false
+        post :create
+      end
+
+      specify do
+        response.should be_redirect
+        flash[:notice] = "Task Created."
+      end
+    end
+
+  end
+
 end
